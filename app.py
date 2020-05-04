112a4b029b660512e7ebb8b4fe13adac0f939309
Charlie <itscharlieliu@gmail.com>
2020-05-03 19:55:08 -0700

    Added rest endpoint POST and GET

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        json = request.get_json()
+        return jsonify({'you sent': json}), 200
+    else:
+        return jsonify({"about": "Hello world"})
 
 
 if __name__ == '__main__':
